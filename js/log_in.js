f435b2bd2aff5f81f942feeb7aafdc897114baef
riddhi <riddhipatel980511@gmail.com>
2021-01-17 23:18:18 +0530

    third commit

--- js/log_in.js
+++ js/log_in.js
@@ -53,52 +53,6 @@ $(document).ready(function () {
             }) // success
 
 
-
-            // function admin($username,$password){
-
-
-            //     $.ajax({
-
-            //         method: "GET",
-            //         url: "http://localhost:3000/admin",
-            //         // contentType: "application/json; charset=utf-8",
-    
-    
-            //         success: function (data, status) {
-            //             $flag = 0;
-                        
-            //             for ($i = 0; $i < data.length; $i++) {
-    
-            //                 if (data[$i].username == $username && data[$i].password == $password) {
-                              
-            //                     $flag = 0;
-            //                     $(window).attr('location', '../html/admin_dash.html');
-            //                     $.session.set('username',$username);
-            //                     break;
-    
-            //                 }
-                            
-            //                 else{
-            //                     $flag = 1;
-            //                 }
-                            
-            //             }
-    
-            //             if ($flag == 1) {
-            //                 alert("Username Incorrect or Password Didn't Match!!!");
-            //                 $(window).attr('location', '../html/login.html');
-            //             }
-                        
-            //         },
-            //         error: function (data, status) {
-    
-            //             alert('error');
-            //         },
-                   
-    
-            //     })
-
-            // }
         } // ajax
 
 
